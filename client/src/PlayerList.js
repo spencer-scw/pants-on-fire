14b6b0c96415ec214b0f6a07e98e4bf524cec405
Sarah Smalley <bertandfrogs@gmail.com>
2024-08-04 11:44:28 -0600

    added player list component

--- client/src/PlayerList.js
+++ client/src/PlayerList.js
@@ -1,12 +1,13 @@
 import './PlayerList.css';
 
 function PlayerList() {
-	// TODO: get list of players
-	var playerNames = ['Jeff', 'Geoff', 'Djeff', 'Jeffrey', 'Chef']
+	// TODO: get list of players from backend
+	var playerNames = ['Jeff', 'Geoff', 'Djeff', 'Gef', 'Chef']
 	const playerHTML = playerNames.map((name) => <div className='player'>{name}</div>)
 
   return (
 	<div className="PlayerList">
+		<div className="player_label">Players:</div>
 		<div className="list">
 			{playerHTML}
 		</div>
